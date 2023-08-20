bcf5e0a9843407876a4a8953d4f0a10bb278043e
Sarah Thomas <sbade92@gmail.com>
2023-08-19 18:55:59 -0700

    Finished Video Player Challenge

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -7,6 +7,7 @@ const toggle = document.querySelector('.toggle');
 const progress = document.querySelector('.progress');
 const skipButtons = document.querySelectorAll('[data-skip]');
 const ranges = document.querySelectorAll('.player__slider');
+const fullScreen = document.querySelector('.fullscreen2');
 
 // Functions
 
@@ -27,8 +28,32 @@ function updateButton() {
 };
 
 function skip(){
-    console.log('skipping');
+    video.currentTime += parseFloat(this.dataset.skip);
 }
+
+function handleRangeUpdate() {
+    video[this.name] = this.value; 
+};
+
+function handleProgress(){
+    const percent = ((video.currentTime)/(video.duration) * 100);
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime; 
+}
+
+function fullScreening() {
+    if (player.classList.contains('fullscreen')) {
+        player.classList.remove('fullscreen');
+        player.classList.remove('-webkit-full-screen');
+    } else {
+        player.classList.add('fullscreen');
+        player.classList.add('-webkit-full-screen');
+    }
+};
 
 // Event Listeners
 
@@ -36,5 +61,18 @@ toggle.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+fullScreen.addEventListener('click', fullScreening);
 
 skipButtons.forEach(button => button.addEventListener('click', skip)); 
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
